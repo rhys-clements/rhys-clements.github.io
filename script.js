e122fc35cc49a47cb364f4a18d37e9b2fbb2ba96
Rhys Clements <rhys.clements@outlook.com>
2022-08-20 00:34:20 +0100

    Scramble object approach

--- script.js
+++ script.js
@@ -1,51 +1,55 @@
-var original;
-var encrypted;
 var characters = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
                   'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',
                   'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l',
                   'z', 'x', 'c', 'v', 'b', 'n', 'm',
                   '!', '£', '$', '%'];
 
-original = document.getElementById("encrypt").innerHTML;
-document.getElementById("encrypt").innerHTML = encrypt(original);
-
-function encrypt(text){
-  console.log(text)
-  text = text.split('');
-  for(var i = 0; i < text.length; i++){
-    if(characters.includes(text[i])){
-      text[i] = characters[Math.floor(Math.random() * characters.length)];
-    }
+class Scrambled{
+  constructor(element){
+    this.element = element;
+    //this.delay = delay;
+    this.text = element.innerHTML;
+    this.encrypted = this.encrypt(this.text);
+    this.timer = setInterval(this.update.bind(this),20);
   }
-  text = text.join('');
-  console.log(text)
-  return text
-}
 
-function decrypt(original, encrypted){
-  original = original.split('');
-  encrypted = encrypted.split('');
-  for(var i = 0; i < original.length; i++){
-    if(encrypted[i] != original[i]){
-      encrypted[i] = characters[Math.floor(Math.random() * characters.length)];
-    }
-  }
-  original = original.join('');
-  encrypted = encrypted.join('');
-  return encrypted;
-
-}
-
-function iterate(original, encrypted){
-  var timer = setInterval(
-    function(){
-      encrypted = decrypt(original, encrypted);
-      document.getElementById("encrypt").innerHTML = encrypted;
-
-      if(encrypted == original){
-        clearInterval(timer);
+  encrypt(text){
+    text = text.split('');
+    for(var i = 0; i < text.length; i++){
+      if(characters.includes(text[i])){
+        text[i] = characters[Math.floor(Math.random() * characters.length)];
       }
-    }, 50);
+    }
+    text = text.join('');
+    return text
+  }
+
+  decrypt(original, encrypted){
+    original = original.split('');
+    encrypted = encrypted.split('');
+    for(var i = 0; i < original.length; i++){
+      if(encrypted[i] != original[i]) {
+        encrypted[i] = characters[Math.floor(Math.random() * characters.length)];
+      }
+    }
+    original = original.join('');
+    encrypted = encrypted.join('');
+
+    return encrypted
+  }
+
+  update(){
+    this.encrypted = this.decrypt(this.text,this.encrypted);
+    this.element.innerHTML = this.encrypted;
+
+    if(this.encrypted == this.text){
+      clearInterval(this.timer);
+    }
+  }
+
 }
 
-iterate(original, document.getElementById("encrypt").innerHTML);
+var elements = document.getElementsByClassName('scramble');
+for (var i = 0; i < elements.length; i++) {
+  let e = new Scrambled(elements[i]);
+}
