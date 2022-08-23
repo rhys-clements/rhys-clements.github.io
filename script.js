1ec0fb73d4cb60cd92b645f511bd1caa34a69665
Rhys Clements <rhys.clements@outlook.com>
2022-08-23 19:11:01 +0100

    Email Harvesting Prevention

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-var characters = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
-                  'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',
+var characters = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',
                   'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l',
-                  'z', 'x', 'c', 'v', 'b', 'n', 'm',
-                  '!', '£', '$', '%'];
+                  'z', 'x', 'c', 'v', 'b', 'n', 'm'];
 
 class Scrambled{
   constructor(element){
